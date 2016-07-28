3eaf2c8503b60eac705376d7e1e105e697a06133
Schyler Hosler <schylerhosler@gmail.com>
2016-07-28 13:27:07 -0400

    Initial commit:

---
+++ lib/error-school.rb
@@ -0,0 +1,26 @@
+# code here!
+class School
+
+  roster = {}
+
+  def initialize(school)
+    @school = school
+  end
+
+  def add_student
+  end
+
+  def roster
+  end
+
+end
+
+#rspec tests not working... telling me I have 2 arguments for everything
+
+#school = School.new("Bayside High School")
+#school.roster
+# => {}
+
+#school.add_student("Zach Morris", 9)
+#school.roster
+# => {9 => ["Zach Morris"]}
--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+class School
+
+  def roster
+    @roster #{ |hash, key| hash[key] = [] }
+  end
+
+  # def roster=(roster)
+  #   @@roster = roster
+  # end
+
+  def initialize(school)
+    @school = school
+    @roster = Hash.new
+  end
+
+  def add_student(name, school_year)
+    if @roster.include?(school_year)
+      @roster[school_year] << name
+    else
+      @roster[school_year] = Array.new
+      @roster[school_year] << name
+    end
+    #return @roster
+  end
+
+  def grade(school_year)
+    return @roster[school_year]
+  end
+
+  def sort
+    @roster.each do |array, items|
+      @roster[array] = items.sort
+    end
+    return Hash[@roster.sort]
+  end
+
+end
